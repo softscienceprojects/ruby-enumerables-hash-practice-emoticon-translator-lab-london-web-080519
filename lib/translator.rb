cf78f50293e04e808fcf92c157c592ec7960a661
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-27 17:59:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -45,6 +45,8 @@ def get_english_meaning(path, emoticon)
   lookup.each do |k,v|
     if k == emoticon
       answer = v
+      if k != emoticon
+        answer = "Sorry, that emoticon was not found"
     end
   end
   answer
