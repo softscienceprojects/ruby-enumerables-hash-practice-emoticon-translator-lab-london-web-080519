16fbf6e604b6d7d51bffb34e6f331f37be9d6c2a
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-27 17:39:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(path, emoticon)
   #load_library(path)["get_emoticon"].include?(emoticon)
   answer = ""
   load_library(path)["get_emoticon"].each do |k, v|
-    if k == emoticon
+    if k.include?(emoticon)
        answer = v
     else
     answer = "Sorry, that emoticon was not found"
